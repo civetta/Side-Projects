ea49e0a8b2a76827a584b759815bf553486c1585
Kelly-Anne Richardson <civettaxx@gmail.com>
2022-06-29 13:38:27 +0000

    Update Practice_Github.R

--- Practice_Github.R
+++ Practice_Github.R
@@ -1,4 +1,4 @@
 #Hi this file is going to github for the first time 
 initial_variable <- 123
 keep <- "I want to keep this variable"
-dont <- "I don't want to keep this variable"
+dont <- "I don't want to keep this variable"dont <- "I don't want to keep this variable"
