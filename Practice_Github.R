96398210058c33cef695cac9830cc45a8ffd30d8
Kelly-Anne Richardson <civettaxx@gmail.com>
2022-06-29 13:58:48 +0000

    Update Practice_Github.R

--- Practice_Github.R
+++ Practice_Github.R
@@ -1,5 +1,2 @@
-#Hi this file is going to github for the first time 
-initial_variable <- 123
-keep <- "I want to keep this variable"
-practice <-"What is this?"
-lose<-"Let's Lose this change!"
+#Testing for Github 
+var_1 < 1
